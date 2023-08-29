d8842773f0c2be9c250e82f343d7d699c496e643
KrasniKot <emanueltrias9@gmail.com>
2023-08-29 23:08:18 +0000

    tsk 4: done; task 3: pycodestyle fixed

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -5,6 +5,6 @@ import asyncio
 waitom = __import__("0-basic_async_syntax").wait_random
 
 
-def task_wait_random(max_delay: int ) -> asyncio.Task:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """Returns an ascyncio Task"""
     return asyncio.create_task(waitom(max_delay))
---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""This module contains almost wait_n()"""
+
+from asyncio import as_completed
+from typing import List
+waitom = __import__('3-tasks.py').task_wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Returns a list of all the delays"""
+
+    dlays = []
+    tasks = []
+
+    for time in range(n):
+        tasks.append(waitom(max_delay))
+    for task in as_completed(tasks):
+        dlays.append(await(task))
+    return dlays
