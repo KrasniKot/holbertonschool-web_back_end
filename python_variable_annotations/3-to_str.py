4bcfd05cf4bf1f20dfde8c199f616974408e5004
KrasniKot <emanueltrias9@gmail.com>
2023-08-29 02:47:19 +0000

    project in progress

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module contains a function"""
+
+
+def to_str(n: float) -> str:
+    """Returns the str representation of a float"""
+    return str(n)
---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This module contains sum_list"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]): -> float:
+    """Returns the addition of a list"""
+    return float(sum(input_list))
