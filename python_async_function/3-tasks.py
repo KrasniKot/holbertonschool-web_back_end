154311e22cee50eb3246ec16de46f9d51a19eb43
KrasniKot <emanueltrias9@gmail.com>
2023-08-29 23:03:40 +0000

    task 3: done

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This mmodule contains task_wait_random()"""
+
+import asyncio
+waitom = __import__("0-basic_async_syntax").wait_random
+
+
+def task_wait_random(max_delay: int ) -> asyncio.Task:
+    """Returns an ascyncio Task"""
+    return asyncio.create_task(waitom(max_delay))
