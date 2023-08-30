d67202d1ca2e96ac8c0bb8470f4deb6403c35e48
KrasniKot <emanueltrias9@gmail.com>
2023-08-30 06:36:56 -0700

    task 1: pycodestyle

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """This module contains async_comprehension"""
 
+from typing import List
 asgen = __import__("0-async_generator").async_generator
-from asyncio import run
-from typing import Generator
 
-async def async_comprehension() -> Generator[float, None, None]:
+
+async def async_comprehension() -> List[float]:
     """Returns 10 random numbers"""
-    ns = [n async for n in asgen()]    
-    return ns   
+    ns = [n async for n in asgen()]
+    return ns
