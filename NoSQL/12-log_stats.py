161143b849a4601e5ccce311865f9547515da25d
krasnikot <emanueltrias9@gmail.com>
2023-09-05 01:26:53 -0700

    task 12: done

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""This module provides some stats about Nginx logs in the mongo db"""
+
+from pymongo import MongoClient
+
+
+client = MongoClient("mongodb://127.0.0.1:27017")
+coll = client.logs.nginx
+
+method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+print(coll.count_documents({}))
+print("Methods:")
+
+for m in method:
+    print(f"\t{m}: {coll.count_documents({'method': m})}")
+
+print(coll.count_documents({"method": "GET", "path": "/status"}))
