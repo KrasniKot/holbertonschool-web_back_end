9e1b4703e5305dacd7dc1b6c6b5447f78ba603c4
KrasniKot <emanueltrias9@gmail.com>
2023-09-06 06:21:33 -0700

    task 0: readme and type anottations added

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """This module contains index_range()"""
 
+from typing import Tuple
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Returns a tuple (start index, end index)"""
     return (page * page_size - page_size, page_size * page)
