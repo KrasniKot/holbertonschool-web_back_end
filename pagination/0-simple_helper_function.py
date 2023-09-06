54c756ee8029f09a45888ae4a11bc5e75ea1040f
KrasniKot <emanueltrias9@gmail.com>
2023-09-06 06:28:30 -0700

    task 1: in progress

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,6 +3,7 @@
 
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Returns a tuple (start index, end index)"""
     return (page * page_size - page_size, page_size * page)
