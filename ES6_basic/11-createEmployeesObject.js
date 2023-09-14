c383c3329fb952f17d420420a0b07b8c2469a278
KrasniKot <emanueltrias9@gmail.com>
2023-09-14 14:20:05 -0300

    task 10: done

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,2 +1,2 @@
-export default function createEmployeesObject(departmentName, employees) { return {[departmentName]: [employees]}
+export default function createEmployeesObject(departmentName, employees) { return {[departmentName]: [employees]}; }
 }
