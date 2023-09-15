b79e0fd04356bc18e2165054309422449bf081a6
KrasniKot <emanueltrias9@gmail.com>
2023-09-15 16:19:58 -0300

    task 4 eh

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -12,7 +12,12 @@ export default class Pricing {
 
   set amount(amount) { this._amount = amount; }
 
-  set currency(currency) { this._currency = currency; }
+  set currency(currency) {
+    if (typeof currency === Currency) {
+      this._currency = currency;
+    }
+    else { throw TypeError('currency must be a Currency') }
+  }
 
   displayFullPrice() { return `${this._amount} ${this.currency._name} (${this.currency._code})`; }
 }
