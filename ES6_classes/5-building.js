b57ad8c2985c30233a3505eb7da975853e1d94a4
KrasniKot <emanueltrias9@gmail.com>
2023-09-15 19:34:57 -0300

    task 5 done

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,13 +1,13 @@
 export default class Building {
   constructor(sqft) {
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+
     this.sqft = sqft;
   }
 
   get sqft() { return this._sqft; }
 
   set sqft(sqft) { this._sqft = sqft; }
-
-  static evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
 }
