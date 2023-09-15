86de2fd95adfa68f600df8bf44f8b911a3e4be52
KrasniKot <emanueltrias9@gmail.com>
2023-09-15 16:13:11 -0300

    task 4: done

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,18 @@
+import Currency from "./3-currency.js";
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() { return this._amount; }
+
+  get currency() { return this._currency; }
+
+  set amount(amount) { this._amount = amount; }
+
+  set currency(currency) { this._currency = currency; }
+
+  displayFullPrice() {return `${this._amount} ${this.currency._name} (${this.currency._code})`;}
+}
