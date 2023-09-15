b8a5f29353eb51f7b71c415b2e239bc8c59ca43e
KrasniKot <emanueltrias9@gmail.com>
2023-09-15 19:55:33 -0300

    task 6 done

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -3,8 +3,11 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
+
     this._floors = floors;
   }
+
+  get sqft() { return this._sqft; }
 
   get floors() { return this._floors; }
 
