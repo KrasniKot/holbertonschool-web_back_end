e4550f373a8172ec8e8ca15d1fa013159051fc91
KrasniKot <emanueltrias9@gmail.com>
2023-09-15 23:52:35 -0300

    done

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -23,7 +23,7 @@ export class StudentHolberton {
   get holbertonClass() { return this._holbertonClass; }
 
   get fullStudentDescription() {
-    return `${this._firstName} ${lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+    return `${this._firstName} ${this.lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
 
