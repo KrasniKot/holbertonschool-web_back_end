90c67e8fa8f199ca32ce16d0d59af007c5cc53ed
KrasniKot <emanueltrias9@gmail.com>
2023-09-17 15:11:34 -0300

    task 100 linter

--- ES6_basic/100-createIteratorObject.js
+++ ES6_basic/100-createIteratorObject.js
@@ -1,8 +1,10 @@
 export default function createIteratorObject(report) {
   const empList = [];
 
+  /* eslint-disable */
   for (const dep in report["allEmployees"]) {
     for (const emp of report["allEmployees"].dep) { empList.push(emp); }
   }
+
   return empList;
 }
