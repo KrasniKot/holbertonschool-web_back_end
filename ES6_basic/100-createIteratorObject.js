00afba8a10c0d1de253a84c6af088426f6ae1c70
KrasniKot <emanueltrias9@gmail.com>
2023-09-17 15:23:24 -0300

    code fix

--- ES6_basic/100-createIteratorObject.js
+++ ES6_basic/100-createIteratorObject.js
@@ -2,8 +2,8 @@ export default function createIteratorObject(report) {
   const empList = [];
 
   /* eslint-disable */
-  for (const dep in report["allEmployees"]) {
-    for (const emp of report["allEmployees"].dep) { empList.push(emp); }
+  for (const dep in report.allEmployees) {
+    for (const emp of report.allEmployees[[dep]) { empList.push(emp); }
   }
 
   return empList;
