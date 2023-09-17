ec95f00333b9ac760781a0a66010f2197f8a7877
KrasniKot <emanueltrias9@gmail.com>
2023-09-17 15:09:52 -0300

    linter

--- ES6_basic/100-createIteratorObject.js
+++ ES6_basic/100-createIteratorObject.js
@@ -2,7 +2,7 @@ export default function createIteratorObject(report) {
   const empList = [];
 
   for (const dep in report["allEmployees"]) {
-    for (const emp of report."allEmployees".dep) { empList.push(emp); }
+    for (const emp of report["allEmployees"].dep) { empList.push(emp); }
   }
   return empList;
 }
