00054b0214e30f33f02d78e9cc60041609644fb5
KrasniKot <emanueltrias9@gmail.com>
2023-10-04 15:44:16 +0000

    task 6: returned array fixed

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,6 +5,6 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((response) => [
       { status: response[0].status, value: response[0].value },
-      { status: response[1].status, value: response[1].status },
+      { status: response[1].status, value: response[1].value },
     ]);
 }
