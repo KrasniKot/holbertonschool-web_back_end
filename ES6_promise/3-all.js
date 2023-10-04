305dc15c9f60083bd304a6614f12bfc8947d3669
KrasniKot <emanueltrias9@gmail.com>
2023-10-04 14:19:43 +0000

    task 3: response list fixed(?)

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,9 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
-/* eslint-disable no-unused-vars */
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()]).then((results) => {
+  return Promise.all([uploadPhoto(), createUser()]).then([pres, ures]) => {
     console.log(results[0].body, results[1].firstName, results[1].lastName);
   }).catch((err) => { console.log('Signup system offline'); });
 }
-/* eslint-enable no-unused-vars */
