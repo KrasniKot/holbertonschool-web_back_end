18a50ad0de0c6cb6adefcb7816f42ed04481890d
KrasniKot <emanueltrias9@gmail.com>
2023-10-04 14:24:57 +0000

    task 3: response list format fixed

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()]).then([pres, ures]) => {
-    console.log(results[0].body, results[1].firstName, results[1].lastName);
-  }).catch((err) => { console.log('Signup system offline'); });
+  return Promise.all([uploadPhoto(), createUser()]).then(([pres, ures]) => {
+    console.log(pres.body, ures.firstName, ures.lastName);
+  }).catch(() => { console.log('Signup system offline'); });
 }
