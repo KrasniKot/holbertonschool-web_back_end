32dfe4a97a99b7460b5d25f19421c1488df2c2ae
KrasniKot <emanueltrias9@gmail.com>
2023-10-04 14:32:23 +0000

    task 3: console.log() fixed

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()]).then(([pres, ures]) => {
-    console.log(pres.body, ures.firstName, ures.lastName);
+  return Promise.all([uploadPhoto(), createUser()]).then((values) => {
+    console.log(`values[0].body, values[1].firstName, values[1].lastName`);
   }).catch(() => { console.log('Signup system offline'); });
 }
