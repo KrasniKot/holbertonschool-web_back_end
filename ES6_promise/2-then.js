3ca63a187ff9acc954490889945de9bdf858a2bb
KrasniKot <emanueltrias9@gmail.com>
2023-10-04 14:06:31 +0000

    task 2: fixed

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,8 +1,11 @@
-/* eslint-disable no-unused-vars */
 export default function handleResponseFromAPI(promise) {
-  console.log('Got a response from the API');
-  return new Promise((resolve, reject) => {
-    if (success) { resolve({ status: 200, body: 'success' }); } else { reject(new Error()); }
-  });
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error('');
+    });
 }
-/* eslint-enable no-unused-vars */
