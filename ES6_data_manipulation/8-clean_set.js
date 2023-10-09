e12a0653f868215335d890008e1ca3e08e093f97
KrasniKot <emanueltrias9@gmail.com>
2023-10-09 04:14:54 +0000

    task 10: done

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   let result = '';
   if (!startString || !startString.length) return result;
-  for (const value of set) { if (value.startsWith(startString)) { result += `${value.slice(startString.length)}-`; } }
+  for (const value of set) { if (value && value.startsWith(startString)) { result += `${value.slice(startString.length)}-`; } }
   return result.slice(0, result.length - 1);
 }
